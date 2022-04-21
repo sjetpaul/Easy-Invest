53933c2edcc28a3b50d8eddf18d86a8480f2fd6d
unknownboys2022 <103655879+unknownboys2022@users.noreply.github.com>
2022-04-21 10:27:16 +0530

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,11 +1,14 @@
 <?php 
+//database Name
+    $database = "register_user";
 //connect to database
-	$con = mysqli_connect("localhost","root","","admin");
-
-	// chech connection
+	$con = mysqli_connect("localhost","root","",$database); 
 		if(mysqli_connect_errno()){
 			echo "Failed to connect Mysql:" . mysqli_connect_errno();
 		}
 	// Table Names
-		$tb_login = "login";
+		$tb_login = "register_user1";
+
+	// Table Name for risk_analyze
+		$tb_login2 = "risk_data";
 ?>
