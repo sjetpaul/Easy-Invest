dd3a28796d111e59a7eb87a2345d2cb13b75aff8
unknownboys2022 <103655879+unknownboys2022@users.noreply.github.com>
2022-04-23 00:23:12 +0530

    Add files via upload

---
+++ temp/jsondata.php
@@ -0,0 +1,19 @@
+<?php
+$json = file_get_contents('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C7d%2C30d%2C1y');
+
+$data = json_decode($json, true);
+
+$coin = array();
+foreach ($data as $key1 => $value1) {
+    foreach($value1 as $key2 => $value2){
+        if($key2=="symbol" && $value2=="btc"){
+            foreach($value1 as $key => $value){
+                $coin[$key]=$value;
+            }
+        break;
+        }
+    }
+}
+echo var_dump($coin);
+
+?>
