1e0b85ead91f8c07ded67fae74c1a2618c073e57
unknownboys2022 <103655879+unknownboys2022@users.noreply.github.com>
2022-04-24 13:27:24 +0530

    Add files via upload

---
+++ page/sidebar.php
@@ -0,0 +1,78 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="index.php">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <!-- <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-journal-text"></i><span>Risk Profile</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="risk-summary.php">
+              <i class="bi bi-circle"></i><span>Risk Summary</span>
+            </a>
+          </li>
+          <li>
+            <a href="risk-analyze.php">
+              <i class="bi bi-circle"></i><span>Update</span>
+            </a>
+          </li>
+        </ul>
+      </li> -->
+      <!-- End Risk Profile -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="risk-analyze.php">
+          <i class="bx bxs-notepad"></i>
+          <span>Risk Analyze</span>
+        </a>
+      </li><!-- End Search Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="search.php">
+          <i class="bi bi-search"></i>
+          <span>Search</span>
+        </a>
+      </li><!-- End Search Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="crypto_list.php">
+          <i class="bi bi-google"></i>
+          <span>Crypto List</span>
+        </a>
+      </li>
+
+      <li class="nav-heading">Personal</li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="profile.php">
+          <i class="bi bi-person"></i>
+          <span>Profile</span>
+        </a>
+      </li><!-- End Profile Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="contact.php">
+          <i class="bi bi-envelope"></i>
+          <span>Contact</span>
+        </a>
+      </li><!-- End Contact Page Nav -->
+
+
+      <!-- <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-blank.php">
+          <i class="bi bi-file-earmark"></i>
+          <span>Blank</span>
+        </a>
+      </li> -->
+       
+      <!-- End Blank Page Nav -->
+
+    </ul>
+
+  </aside>
